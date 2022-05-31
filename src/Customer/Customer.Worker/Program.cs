848e0eef4e1f3408a92091335ffab332f4f590fc
Kenneth Jakobsen <kennethjakobsen@Kenneths-MBP.lan>
2022-05-31 13:45:21 +0200

    Added Handling of messages

--- src/Customer/Customer.Worker/Program.cs
+++ src/Customer/Customer.Worker/Program.cs
@@ -1,11 +1,35 @@
-﻿using Customer.Worker;
+﻿using Customer.Messages;
+using Customer.Worker;
+using Customer.Worker.Handlers;
+using Rebus.Config;
+using Rebus.Handlers;
+using Rebus.Routing.TypeBased;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
-    })
-    .Build();
+        services.AutoRegisterHandlersFromAssemblyOf<CreateNewCustomerHandler>();
+        services.AddSingleton(serviceProvider => {
+            
+            IConfiguration config = serviceProvider.GetRequiredService<IConfiguration>();
+            string rabbit = config.GetValue<string>("ConnectionStrings:RabbitMq");
+            string input = config.GetValue<string>("Messaging:InputQueue");
+            return new WorkerConfiguration(rabbit, input);
+        });
+        services.AddRebus((configure, provider) =>
+        {
+            var wc = provider.GetRequiredService<WorkerConfiguration>();
+            
+            return configure
+            .Transport(t => t.UseRabbitMq(wc.RabbitMq, wc.InputQueueName))
+            .Options(o => o.SetNumberOfWorkers(5))
+            .Routing(r => r.TypeBased().MapAssemblyOf<CreateNewCustomerCommand>(wc.InputQueueName));
+        });
+        
+    });
+   var host = builder.Build();
 
 await host.RunAsync();
 
---
+++ src/Customer/Customer.Worker/WorkerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Customer.Worker
+{
+	public class WorkerConfiguration
+	{
+        public string RabbitMq { get; set; }
+        public string InputQueueName { get; set; }
+
+        public WorkerConfiguration(string rabbitMq, string inputQueueName)
+        {
+            RabbitMq = rabbitMq;
+            InputQueueName = inputQueueName;
+        }
+    }
+}
+
