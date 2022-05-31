6ebd092ac35adba1ab2ce526a3eb97f9e8b0eb53
Kenneth Jakobsen <kennethjakobsen@Kenneths-MBP.lan>
2022-05-31 13:51:23 +0200

    Added Handling of messages

---
+++ src/Customer/Customer.Worker/Handlers/CreateNewCustomerHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Customer.Messages;
+using Rebus.Handlers;
+
+namespace Customer.Worker.Handlers
+{
+    public class CreateNewCustomerHandler : IHandleMessages<CreateNewCustomerCommand>
+    {
+        private readonly ILogger<Worker> logger;
+
+        public Task Handle(CreateNewCustomerCommand message)
+        {
+            logger.LogInformation("Message Received - Name: " + message.Name);
+            return Task.CompletedTask;
+        }
+
+        public CreateNewCustomerHandler(ILogger<Worker> logger)
+		{
+            this.logger = logger;
+        }
+	}
+}
+
